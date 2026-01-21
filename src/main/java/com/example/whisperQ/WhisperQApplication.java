e562481c1363b3ef3b84f52da9f1d440493eef5b
juanpark <juanpark@me.com>
2026-01-21 14:43:34 +0900

    Configure Korean timezone (Asia/Seoul) for timestamps
    - Set JVM default timezone to Asia/Seoul in application startup
    - Configure Jackson to serialize dates in Asia/Seoul timezone

--- src/main/java/com/example/whisperQ/WhisperQApplication.java
+++ src/main/java/com/example/whisperQ/WhisperQApplication.java
@@ -1,8 +1,12 @@
 package com.example.whisperQ;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.annotation.PostConstruct;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +14,12 @@ public class WhisperQApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhisperQApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Set JVM default timezone to Korea
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
